20c7c84635bf280d1db759925a861f627129755b
Airat56 <ajratnabiev024@gmail.com>
2025-10-05 21:23:55 +0500

    Add methods to Point and examples of their use

--- Laba 2/main.cpp
+++ Laba 2/main.cpp
@@ -21,13 +21,36 @@ public:
     }
 
     ~Point() {
-        printf("Point: x = %d, y = %d\n", x, y);
+        print();
         printf("~Point()\n");
+    }
+
+    // реализация метода сразу в определении
+    void set(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    void move(int dx, int dy) {
+        x = x + dx;
+        y = y + dy;
+    }
+
+    void reset();
+
+    void print() {
+        printf("Point: x = %d, y = %d\n", x, y);
     }
 
 private:
     int x, y;
 };
+
+// реализация метода после определения
+void Point::reset() {
+    x = 0;
+    y = 0;
+}
 
 int main() {
     {
@@ -36,14 +59,32 @@ int main() {
         Point p2(5, 10);
         Point p3(p2);
     }
-    printf("\n");
+    printf("----------------------------------------------------------------------\n");
 
-    // пример создания (с помощью различных конструкторов) и удаления динамически создаваемых объектов
-    Point *p = new Point;
-    Point *p2 = new Point(5, 10);
-    Point *p3 = new Point(*p2);
+    {
+        // пример создания (с помощью различных конструкторов) и удаления динамически создаваемых объектов
+        Point *p = new Point;
+        Point *p2 = new Point(5, 10);
+        Point *p3 = new Point(*p2);
 
-    delete p;
-    delete p2;
-    delete p3;
+        delete p;
+        delete p2;
+        delete p3;
+    }
+    printf("----------------------------------------------------------------------\n");
+
+    {
+        // пример использования методов объектов
+        Point *p = new Point;
+        p->set(5, 10);
+        p->print();
+        p->move(4, 7);
+        p->print();
+        p->reset();
+        p->print();
+        delete p;
+    }
+    printf("----------------------------------------------------------------------\n");
+
+    return 0;
 }
